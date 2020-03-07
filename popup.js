186203e99eafddb874aa13b4fe3048ddfda82301
Jonas Gustafsson <jonas.gustafsson@leasify.se>
2020-03-07 19:24:43 +0100

    updated popup

--- popup.js
+++ popup.js
@@ -1,15 +1,6 @@
-let changeColor = document.getElementById('changeColor');
+let ProfileSpan = document.getElementById('profile');
 
-chrome.storage.sync.get('color', function (data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
+chrome.storage.sync.get(['profile'], function(result) {
+    ProfileSpan.innerText = result.profile
+    console.log('Value currently is ' + result.key);
 });
-
-changeColor.onclick = function (element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
-};
